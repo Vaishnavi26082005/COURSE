2f5bc6cdb96cdd53aa5af5b158401bb23ef2e448
vaishnavi <vaishnavicool1234@gmail.com>
2025-05-24 02:28:47 +0530

    zod validation

--- BACKEND/controllers/user.controller.js
+++ BACKEND/controllers/user.controller.js
@@ -1,8 +1,26 @@
 import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import {z} from "zod";
 
 export const signup = async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
-    
+     const userSchema = z.object({
+        firstName: z.string().min(2, { message: "First name is required" }),
+        lastName: z.string().min(2, { message: "Last name is required" }),
+        email: z.string().email({ message: "Invalid email address" }),
+        password: z.string().min(6, { message: "Password must be at least 6 characters long" }),})
+
+    const validation = userSchema.safeParse(req.body);
+    if (!validation.success) {
+        return res.status(400).json({
+            message: "Validation error",
+            errors: validation.error.issues.map(err=>err.message),
+        });
+    }
+
+    //hashed passsword
+    const hashedPassword= await bcrypt.hash(password, 10);
+
    try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
@@ -10,7 +28,8 @@ export const signup = async (req, res) => {
             message: "user already exists",
         });
     }
-    const newUser= new User ({firstName,lastName,email,password});
+    const newUser= new User ({firstName,lastName,email,
+    password: hashedPassword});
     await newUser.save();
     res.status(201).json({message:"user created successfully",newUser});
    } catch (error) {
