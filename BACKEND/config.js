93c6c29d7ff861a791f8d07decd1da70ca23199e
vaishnavi <vaishnavicool1234@gmail.com>
2025-05-30 08:57:08 +0530

    commit

--- BACKEND/config.js
+++ BACKEND/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 dotenv.config();
 const JWT_USER_PASSWORD=process.env.JWT_USER_PASSWORD
 const JWT_ADMIN_PASSWORD=process.env.JWT_ADMIN_PASSWORD
-const STRIPE_SECRET_KEY="sk_test_51RUG9fIjnqkP0Bf3XedPWMlnTCZeL6P2r8HaYn0ybR2FZjU8tiDuKfx8RnrWV6kpMYeioQqDUxLq7VYbj6mqhlsz00EjD0LtO0"
+const STRIPE_SECRET_KEY=process.env.STRIPE_SECRET_KEY
 
 export default {
     JWT_USER_PASSWORD,
